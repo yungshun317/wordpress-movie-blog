04f1e3b4e12616f584d283fd12c5405b2e8dc6fb
yungshun317 <seraph317@gmail.com>
2023-01-20 09:39:54 +0800

    [FEAT] Create `index.php`, `style.css`, and `screenshot.png` for the WordPress theme

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP Movie Blog</title>
+</head>
+<body>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit officiis possimus ex sequi quo facere perspiciatis rem ratione, in quod illum ab numquam! Aliquam in, non debitis quis aspernatur voluptatum.</p>
+</body>
+</html>
