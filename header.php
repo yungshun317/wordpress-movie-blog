9aa5210dff5ad158acadac5d9607e9332545c5d2
yungshun317 <seraph317@gmail.com>
2023-01-21 10:02:40 +0800

    [FEAT] Create `header.php` and `footer.php` and retrieve them using `get_header` and `get_footer`

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WP Movie Blog</title>
+</head>
+<body <?php body_class(); ?>>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit officiis possimus ex sequi quo facere perspiciatis rem ratione, in quod illum ab numquam! Aliquam in, non debitis quis aspernatur voluptatum.</p>
+<div id="page" class="site">
+    <header>
+        <section class="top-bar">
+            <div class="logo">
+                Logo
+            </div>
+            <div class="searchbox">
+                Search
+            </div>
+        </section>
+        <section class="menu-area">
+            <nav class="main-menu">
+                Menu
+            </nav>
+        </section>
+    </header>
