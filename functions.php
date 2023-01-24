c34581100b6696e0ab5c0a9fc881797c379de4dc
yungshun317 <seraph317@gmail.com>
2023-01-24 15:38:05 +0800

    [FEAT] Add theme support to post thumbnails

--- functions.php
+++ functions.php
@@ -20,5 +20,6 @@ function movie_blog_config() {
         "width" => 1920
     );
     add_theme_support("custom-header", $args);
+    add_theme_support("post-thumbnails");
 }
 add_action("after_setup_theme", "movie_blog_config", 0);
--- index.php
+++ index.php
@@ -12,6 +12,7 @@
                                     ?>
                                     <article>
                                         <h2><?php the_title(); ?></h2>
+                                        <?php the_post_thumbnail(array(275, 275)); ?>
                                         <div class="meta-info">
                                             <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
                                             <p>Categories: <?php the_category(" "); ?></p>
