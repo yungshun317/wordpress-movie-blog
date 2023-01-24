01fbb4ac73332f6588daa43659e509e97c0dbc2c
yungshun317 <seraph317@gmail.com>
2023-01-24 17:10:16 +0800

    [FEAT] Create service boxes using sidebars

--- functions.php
+++ functions.php
@@ -43,4 +43,37 @@ function movie_blog_sidebars() {
             "after_title" => "</h4>"
         )
     );
+    register_sidebar(
+        array(
+            "name" => "Service 1",
+            "id" => "services-1",
+            "description" => "First Service Area",
+            "before_widget" => "<div class='widget-wrapper'>",
+            "after_widget" => "</div>",
+            "before_title" => "<h4 class='widget-title'>",
+            "after_title" => "</h4>"
+        )
+    );
+    register_sidebar(
+        array(
+            "name" => "Service 2",
+            "id" => "services-2",
+            "description" => "First Service Area",
+            "before_widget" => "<div class='widget-wrapper'>",
+            "after_widget" => "</div>",
+            "before_title" => "<h4 class='widget-title'>",
+            "after_title" => "</h4>"
+        )
+    );
+    register_sidebar(
+        array(
+            "name" => "Service 3",
+            "id" => "services-3",
+            "description" => "First Service Area",
+            "before_widget" => "<div class='widget-wrapper'>",
+            "after_widget" => "</div>",
+            "before_title" => "<h4 class='widget-title'>",
+            "after_title" => "</h4>"
+        )
+    );
 }
