205d5a2e0dde6f93e8a663d75e746eac7f7c870e
yungshun317 <seraph317@gmail.com>
2023-01-25 17:47:07 +0800

    [FEAT] Write custom queries and display posts using different parameters with `WP_Query`

--- page-home.php
+++ page-home.php
@@ -32,27 +32,40 @@
                         </div>
                     </section>
                     <section class="home-blog">
+                        <h2>Latest News</h2>
                         <div class="container">
-                            <div class="blog-items">
                                 <?php
-                                    if (have_posts()):
-                                        while (have_posts()): the_post();
+                                    $args = array(
+                                        "post_type" => "post",
+                                        "posts_per_page" => 5,
+                                        "category__in" => array(1),
+                                        "category__not_in" => array(5)
+                                    );
+
+                                    $postlist = new WP_Query($args);
+
+                                    if ($postlist -> have_posts()):
+                                        while ($postlist -> have_posts()): $postlist -> the_post();
                                         ?>
-                                            <article>
-                                                <h2><?php the_title(); ?></h2>
+                                            <article class="latest-news">
+                                                <?php the_post_thumbnail("large"); ?>
+                                                <h3><?php the_title(); ?></h3>
                                                 <div class="meta-info">
-                                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                                                    <p>Categories: <?php the_category(" "); ?></p>
-                                                    <p>Tags: <?php the_tags("", ", "); ?></p>
+                                                    <p>
+                                                        by <span><?php the_author_posts_link(); ?></span>
+                                                        Categories: <span><?php the_category(" "); ?></span>
+                                                        Tags: <?php the_tags("", ", "); ?>
+                                                    </p>
+                                                    <p><span><?php echo get_the_date(); ?></span></p>
                                                 </div>
-                                                <?php the_content(); ?>
+                                                <?php the_excerpt(); ?>
                                             </article>
                                         <?php
                                         endwhile;
+                                        wp_reset_postdata();
                                     else: ?>
                                         <p>Nothing yet to be displayed!</p>
                                 <?php endif; ?>
-                            </div>
                         </div>
                     </section>
                 </main>
