601ec1d5de873e23a890e8d2a33cbe2863ed1216
yungshun317 <seraph317@gmail.com>
2023-01-27 18:55:50 +0800

    [FEAT] Use `get_search_query` in `search.php` to retrieve the contents of the search query variable

--- search.php
+++ search.php
@@ -3,7 +3,9 @@
     <div id="primary">
         <div id="main">
             <div class="container">
+                <h1>Search results for: <?php echo get_search_query(); ?></h1>
                 <?php
+                get_search_form();
                 while (have_posts()):
                     the_post();
                     ?>
