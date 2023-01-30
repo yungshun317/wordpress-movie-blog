58346cd401b62cb3d8e45037d2440457fe5b6297
yungshun317 <seraph317@gmail.com>
2023-01-30 10:37:57 +0800

    [FEAT] Enable editing the copyright notice with the WordPress theme customizer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         <footer class="site-footer">
             <div class="container">
                 <div class="copyright">
-                    <p>Copyright X - All Rights Reserved</p>
+                    <p><?php echo get_theme_mod("set_copyright", "Copyright X - All Rights Reserved"); ?></p>
                 </div>
                 <nav class="footer-menu">
                     <?php wp_nav_menu(array("theme_location" => "movie_blog_footer_menu", "depth" => 1)); ?>
