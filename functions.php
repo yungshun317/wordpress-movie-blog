35205dbbaffd499f3f597b280cd6898a1f48f8c7
yungshun317 <seraph317@gmail.com>
2023-02-26 13:10:07 +0800

    [FEAT] Rename the repository and disable Gutenberg

--- functions.php
+++ functions.php
@@ -82,3 +82,23 @@ function movie_blog_sidebars() {
         )
     );
 }
+
+// Disable Gutenberg on the backend
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+// Disable Gutenberg for widgets
+add_filter( 'use_widgets_block_editor', '__return_false' );
+
+add_action( 'wp_enqueue_scripts', function() {
+    // Remove CSS on the frontend
+    wp_dequeue_style( 'wp-block-library' );
+
+    // Remove Gutenberg theme
+    wp_dequeue_style( 'wp-block-library-theme' );
+
+    // Remove inline global CSS on the front end
+    wp_dequeue_style( 'global-styles' );
+
+    // Remove classic-themes CSS for backwards compatibility for button blocks
+    wp_dequeue_style( 'classic-theme-styles' );
+}, 20);
